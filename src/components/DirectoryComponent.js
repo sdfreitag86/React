cd393ef5226237236f01775b1d35ad018868e859
Scott Freitag <scott.d.freitag@gmail.com>
2022-01-28 09:52:08 -0800

    Bug fix

--- src/components/DirectoryComponent.js
+++ src/components/DirectoryComponent.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { Card, CardImg, CardImgOverlay, CardTitle } from "reactstrap";
 
-function RenderDirectoryItem(campsite, onClick) {
+function RenderDirectoryItem({ campsite, onClick }) {
   return (
     <Card onClick={() => onClick(campsite.id)}>
       <CardImg width="100%" src={campsite.image} alt={campsite.name} />
